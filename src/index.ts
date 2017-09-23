b250d7a8943ddec328afa20894ebf6d52ba97986
Chuck Dries <chuck@chuckdries.com>
2017-09-22 20:31:53 -0700

    be annoying if need arises

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import * as Hs100Api from "hs100-api";
-import * as schedule from "node-schedule";
-// let Hs100Api = require("/hs100-api");
+import Hs100Api from "hs100-api";
+import schedule from "node-schedule";
 
 let client = new Hs100Api.Client();
 let lightplug = client.getPlug({ host: '10.10.11.124' });
@@ -9,13 +8,12 @@ lightplug.getInfo().then(console.log);
 let rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [new schedule.Range(1, 6)]
 rule.hour = 8;
-// rule.minute = 35;
-// rule.second = 10;
+// rule.second = 1; //use this rule in lieu of other two rules to toggle his lights every minute
 
 let state: boolean = true;
 // @ts-ignore: optional parameters are specified in type declaration as non-optional. Not my problem!
 let j = schedule.scheduleJob(rule, function () {
     console.log("wake up carter");
-    // latest = !latest;
+    // state = !state; //toggle, to annoy him.
     lightplug.setPowerState(state);
 });
